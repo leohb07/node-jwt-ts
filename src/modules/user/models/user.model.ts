588b58ddc908d4e26e8778174ebc7ede41f764eb
leohb07 <leohb1501@gmail.com>
2024-03-07 22:31:21 -0300

    feat: add user model

---
+++ src/modules/user/models/user.model.ts
@@ -0,0 +1,40 @@
+import {
+  DocumentType,
+  getModelForClass,
+  index,
+  modelOptions,
+  pre,
+  prop,
+} from "@typegoose/typegoose";
+import bcrypt, { hash } from "bcryptjs";
+
+@index({ email: 1 })
+@pre<User>("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 12);
+})
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class User {
+  @prop()
+  name: string;
+
+  @prop({ unique: true, required: true })
+  email: string;
+
+  @prop({ required: true, minlength: 8, maxlength: 32, select: false })
+  password: string;
+
+  @prop({ default: "user" })
+  role: string;
+
+  async comparePasswords(hashedPassword: string, candidatePassword: string) {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
+  }
+}
+
+export const userModel = getModelForClass(User);
